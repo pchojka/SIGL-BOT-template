9402097f760efb6b1eca09b4444c496224d577b1
Maxime Lizandier <maxime.lizandier@epita.fr>
2021-09-29 19:20:08 +0200

    Add dev id

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ bot = commands.Bot(
     case_insensitive=True  # Commands aren't case-sensitive
 )
 
-bot.author_id = 0000000  # Change to your discord id!!!
+bot.author_id = 238762143485394944  # Change to your discord id!!!
 
 @bot.event
 async def on_ready():  # When the bot is ready
